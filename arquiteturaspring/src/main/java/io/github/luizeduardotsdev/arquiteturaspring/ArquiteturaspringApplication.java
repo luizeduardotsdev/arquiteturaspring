c15a4b379c26efa361b9935480ceb2232d79671e
luiz <luizeduardotsdev@gmail.com>
2025-11-19 08:48:44 -0300

    feat: implementa configuracoes na classe application

--- arquiteturaspring/src/main/java/io/github/luizeduardotsdev/arquiteturaspring/ArquiteturaspringApplication.java
+++ arquiteturaspring/src/main/java/io/github/luizeduardotsdev/arquiteturaspring/ArquiteturaspringApplication.java
@@ -1,13 +1,26 @@
 package io.github.luizeduardotsdev.arquiteturaspring;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class ArquiteturaspringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArquiteturaspringApplication.class, args);
+		// SpringApplication.run(ArquiteturaspringApplication.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(ArquiteturaspringApplication.class);
+        builder.bannerMode(Banner.Mode.OFF);
+        builder.profiles("producao");
+        builder.run(args);
+
+        ConfigurableApplicationContext context = builder.context();
+
+        ConfigurableEnvironment environment = context.getEnvironment();
+        String appName = environment.getProperty("spring.application.name");
+        System.out.println("Application Name: " + appName);
 	}
 
 }

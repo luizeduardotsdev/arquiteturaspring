a0a2fdd83c14593e6978a5ffa00c00f75e9ab5a7
luiz <luizeduardotsdev@gmail.com>
2025-11-23 07:32:26 -0300

    feat: cria o repository do to do

---
+++ arquiteturaspring/src/main/java/io/github/luizeduardotsdev/arquiteturaspring/todo/ToDoRepository.java
@@ -0,0 +1,6 @@
+package io.github.luizeduardotsdev.arquiteturaspring.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToDoRepository extends JpaRepository<ToDoEntity, Integer> {
+}

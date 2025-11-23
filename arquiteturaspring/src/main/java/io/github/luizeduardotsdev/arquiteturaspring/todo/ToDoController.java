3c2abe50511f06c0973568f3e3709beab78ba7d2
luiz <luizeduardotsdev@gmail.com>
2025-11-23 07:44:00 -0300

    feat: cria o metodo post no controller

--- arquiteturaspring/src/main/java/io/github/luizeduardotsdev/arquiteturaspring/todo/ToDoController.java
+++ arquiteturaspring/src/main/java/io/github/luizeduardotsdev/arquiteturaspring/todo/ToDoController.java
@@ -1,5 +1,7 @@
 package io.github.luizeduardotsdev.arquiteturaspring.todo;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ public class ToDoController {
 
     public ToDoController(ToDoService toDoService) {
         this.toDoService = toDoService;
+    }
+
+    @PostMapping
+    public ToDoEntity salvar(@RequestBody ToDoEntity toDoEntity) {
+        return this.toDoService.salvar(toDoEntity);
     }
 }
